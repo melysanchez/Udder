a99fa7a53e35a388ffa9d3eafbac43815fdc0922
Melissa Hidalgo <melyhadev@gmail.com>
2019-10-09 12:51:43 -0700

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Katherynne Hidalgo on 10/9/19.
+//  Copyright © 2019 Katherynne H. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
