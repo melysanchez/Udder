9caf03584ae92f62384fb0a63de3e6fe5fca086c
Melissa Hidalgo <melyhadev@gmail.com>
2019-10-10 10:17:37 -0700

    Add ViewController with method

---
+++ Udder/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Katherynne Hidalgo on 10/10/19.
+//  Copyright © 2019 Katherynne H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
+    }
+}
